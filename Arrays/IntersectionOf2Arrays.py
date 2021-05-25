ab0af747159068c977f5a7e41446dae9bd285784
Nadimpalli Gayathri <nadimpalligayathri0@gmail.com>
2021-05-25 08:09:21 +0530

    Update IntersectionOf2Arrays.py

--- Arrays/IntersectionOf2Arrays.py
+++ Arrays/IntersectionOf2Arrays.py
@@ -1,8 +1,12 @@
 def intersection(list1,list2):
-    list3 = [value for value in list1 if value in list2]
+    list3 = []
+    for value in list1:
+        if value in list2:
+            list3.append(value);
+            list2.remove(value);
     print(list3)
 
-list1 = [1,2,3,4,5]
+list1 = [1,2,3,4,5,4,4,4,4]
 list2 = [2,3,4]
 
-intersection(list1,list2)  
+intersection(list1,list2)
